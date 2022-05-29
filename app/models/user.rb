0dfc785d19e712e2f121101e92aa6ee2603732c7
metacoder87 <gary.miller@gmx.com>
2022-05-29 01:44:30 -0400

    Adds find by credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ApplicationRecord
 
     attr_reader :password
 
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
+    end
+
     def self.generate_session_token
         SecureRandom.urlsafe_base64(16)
     end
