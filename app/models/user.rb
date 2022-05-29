41fb12d64f52f02ab51144670afd17ab55371921
metacoder87 <gary.miller@gmx.com>
2022-05-29 01:33:15 -0400

    Adds reset session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,9 @@ class User < ApplicationRecord
     end
 
     def reset_session_token!
-
+        self.session_token = self.generate_session_token
+        self.save!
+        self.session_token
     end
 
     def ensure_session_token
