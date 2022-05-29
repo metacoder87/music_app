122c9d919fc70672023742a17780a22cd8800cb8
metacoder87 <gary.miller@gmx.com>
2022-05-29 01:31:30 -0400

    Adds generate session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,28 @@ class User < ApplicationRecord
     validates :email, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: { message: "Password can not be blank"}
     validates :password, length: { minimum: 6, allow_nil: true }
+    after_initialize :ensure_session_token
+
+    attr_reader :password
+
+    def self.generate_session_token
+        SecureRandom.urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+
+    end
+
+    def ensure_session_token
+
+    end
+
+    def password=(password)
+
+    end
+
+    def is_password?(password)
+
+    end
+
 end
