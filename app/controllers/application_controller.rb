4b291d66c6cadd124632f861567bedc621952009
metacoder87 <gary.miller@gmx.com>
2022-05-30 01:59:30 -0400

    Adds user methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,22 @@ private
     def logged_in?
         !current_user.nil?
     end
+
+    def logout_user!
+        current_user.reset_session_token!
+        session[:session_token] = nil
+    end
+
+    def login_user!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def require_no_user!
+        redirect_to users_url if current_user
+    end
+
+    def require_user!
+        redirect_to new_session_url if current_user.nil?
+    end
+    
 end
