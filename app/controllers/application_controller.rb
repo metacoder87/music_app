77232b3d343e20e166b30d6f210bcdc721f00be0
metacoder87 <gary.miller@gmx.com>
2022-05-30 01:53:24 -0400

    Adds user methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    helper_method :current_user
+    helper_method :logged_in?
+
+private
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
 end
