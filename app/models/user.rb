1a64e200a36f717b2e3d6903cc4e56c4749e223d
metacoder87 <gary.miller@gmx.com>
2022-08-26 11:54:07 -0400

    Fixes generate session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,11 @@ class User < ApplicationRecord
     end
 
     def self.generate_session_token
-        SecureRandom.urlsafe_base64(16)
+        SecureRandom::urlsafe_base64(16)
     end
 
     def reset_session_token!
-        self.session_token = self.generate_session_token
+        self.session_token = User.generate_session_token
         self.save!
         self.session_token
     end
