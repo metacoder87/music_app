7224a9d54ba34481efdd441cad8feaf3660b98fc
metacoder87 <gary.miller@gmx.com>
2022-12-14 08:58:07 -0500

    Updates album table

--- db/migrate/20221208092754_create_albums.rb
+++ db/migrate/20221208092754_create_albums.rb
@@ -1,7 +1,7 @@
 class CreateAlbums < ActiveRecord::Migration[5.2]
   def change
     create_table :albums do |t|
-      t.string :title, null: false
+      t.string :name, null: false
       t.integer :band_id, null: false
       t.integer :year, null: false
       t.boolean :live, null: false, default: false
