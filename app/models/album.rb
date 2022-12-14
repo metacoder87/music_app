555c08be25507f05cfbd189b9c2ce25a51a5b309
metacoder87 <gary.miller@gmx.com>
2022-12-14 09:22:05 -0500

    Adds track associations to album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ApplicationRecord
   belongs_to :band
+  has_many :tracks, dependent: :destroy
 
   validates :band, :name, :year, presence: true
   validates :live, inclusion: { in: [true, false] }
