a369b7669e8c643416508cf5f19fe62ba11848be
metacoder87 <gary.miller@gmx.com>
2022-12-14 08:56:35 -0500

    Adds album associations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,14 @@
 class Album < ApplicationRecord
-  belongs_to :band_id
+  belongs_to :band
+
+  validates :band, :name, :year, presence: true
+  validates :live, inclusion: { in: [true, false] }
+  validates :name, uniqueness: { scope: :band_id }
+  validates :year, numericality: { minimum: 1400, maximum: 2050 }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.live ||= false
+  end
 end
