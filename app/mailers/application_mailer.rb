34ec946b270206154751164b70094119ddb1cf6e
metacoder87 <gary.miller@gmx.com>
2022-12-15 11:25:15 -0500

    Adds activation email method

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def activation_email(user)
+    @user = user
+    mail(to: user.email, subject: "Welcome to this Music Catalog! Please activate your account.")
+  end
 end
