0e18088664929893cfe1dc9b5a7547972de07e6b
metacoder87 <gary.miller@gmx.com>
2022-12-15 11:24:30 -0500

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_14_135259) do
+ActiveRecord::Schema.define(version: 2022_12_14_153955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,19 @@ ActiveRecord::Schema.define(version: 2022_12_14_135259) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["album_id", "ord"], name: "index_tracks_on_album_id_and_ord", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.boolean "activated", default: false, null: false
+    t.string "activation_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activation_token"], name: "index_users_on_activation_token", unique: true
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
 end
