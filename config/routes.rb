c05407fedccd4a8f2dfd8efbd155e5d4ef045d80
metacoder87 <gary.miller@gmx.com>
2022-12-16 08:36:43 -0500

    Updates resources

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
   end
 
   resources :tracks, only: [:show, :create, :edit, :update, :destroy]
+
+  resources :notes, only: [:create, :edit, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
