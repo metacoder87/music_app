3ebe02f485f27a9e141ff68eac5d6c48a27396e5
metacoder87 <gary.miller@gmx.com>
2022-12-16 08:37:06 -0500

    Adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+User.destroy_all
 Band.destroy_all
 Album.destroy_all
-
+Track.destroy_all
+Note.destroy_all
+4.times do
+    u = User.create(email: Faker::Internet.email, password: Faker::Lorem.word)
+end
 200.times do
     b = Band.create(name: Faker::Music.band)
     5.times do
         a = Album.create(name: Faker::Music.album, band_id: b.id, year: Faker::Number.within(range: 1958..2022), live: Faker::Boolean.boolean(true_ratio: 0.2))
         i = 1
         10.times do
-            t = Track.create(name: Faker::Lorem.word, album_id: a.id, ord: i, bonus: Faker::Boolean.boolean(true_ratio: 0.1), lyrics: Faker::Lorem.paragraph(sentence_count: 15))
+            t = Track.create(name: Faker::Lorem.sentence(word_count: 2), album_id: a.id, ord: i, bonus: Faker::Boolean.boolean(true_ratio: 0.1), lyrics: Faker::Lorem.paragraph(sentence_count: 15))
             i+=1
+            n = Note.create(track_id: t.id, user_id: u.id, content: Faker::Lorem.sentences(number: 1))
         end
     end
 end
